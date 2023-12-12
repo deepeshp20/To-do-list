154e49027e68f0a81f077ed7fe5f5011b91aef57
Deepesh <76147786+deepeshp20@users.noreply.github.com>
2023-12-13 02:40:05 +0530

    Update app.js

--- todolist/app.js
+++ todolist/app.js
@@ -12,7 +12,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+mongoose.connect("mongodb://127.0.0.1:27017/tododb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const itemsSchema = {
   name: String
